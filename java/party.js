8f1098645ab66befb7d746abb82a302444683be3
Kathryne Tao <kathryne@Kathrynes-MacBook-Pro-20.local>
2021-05-20 22:46:25 -0500

    hew commite

--- java/party.js
+++ java/party.js
@@ -1,13 +1,24 @@
 const baseURL = 'https://coolors.co/generate';
 
 let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+document.body.innerHTML += "<h1>Today's date is " + d + "</h1>"
 
-getColors = () => {
-  fetch(baseURL)
-    .then(response => response.json())
-    .then(data => {
 
-    })
 
-  }
+const xhr = new XMLHttpRequest();
+const url = 'https://bar.other/resources/public-data/';
+
+xhr.open('GET', baseURL);
+xhr.onreadystatechange = someHandler;
+xhr.send();
+
+// getColors = () => {
+//   fetch(baseURL)
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log(data)
+//     })
+//
+//   }
+//
+//   getColors()
