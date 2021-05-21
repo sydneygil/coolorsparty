229135d806973bde96ed936f5d1a402b945b92c7
Kathryne Tao <kathryne@Kathrynes-MacBook-Pro-20.local>
2021-05-20 22:56:10 -0500

    hey

--- java/party.js
+++ java/party.js
@@ -5,20 +5,20 @@ document.body.innerHTML += "<h1>Today's date is " + d + "</h1>"
 
 
 
-const xhr = new XMLHttpRequest();
-const url = 'https://bar.other/resources/public-data/';
-
-xhr.open('GET', baseURL);
-xhr.onreadystatechange = someHandler;
-xhr.send();
-
-// getColors = () => {
-//   fetch(baseURL)
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log(data)
-//     })
+// const xhr = new XMLHttpRequest();
+// const url = 'https://bar.other/resources/public-data/';
 //
-//   }
-//
-//   getColors()
+// xhr.open('GET', baseURL);
+// xhr.onreadystatechange = someHandler;
+// xhr.send();
+
+getColors = () => {
+  fetch(baseURL)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+
+  }
+
+getColors()
