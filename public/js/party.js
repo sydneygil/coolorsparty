350eb79c3f924f9caa6f9c18628a0a873105c12c
Kathryne Tao <kathryne@Kathrynes-MacBook-Pro-20.local>
2021-05-31 16:12:52 -0500

    working colors & button

--- public/js/party.js
+++ public/js/party.js
@@ -22,5 +22,48 @@ fetch("/colors")
         return response.json()
     })
     .then(data => {
-        console.log(data)
+
+        data.forEach((color, i) => {
+          color = "background-color: #" + color
+          console.log(color)
+          data[i] = color
+
+        });
+
+        document.querySelector('#color-table').innerHTML = `
+            <thead>
+                <th id="color-1" style="${data[0]}"></th>
+                <th id="color-2" style="${data[1]}"></th>
+                <th id="color-3" style="${data[2]}"></th>
+                <th id="color-4" style="${data[3]}"</th>
+                <th id="color-5" style="${data[4]}"></th>
+            </thead>
+        `;
     })
+
+
+document.querySelector('#generate-button').onclick = ev => {
+    fetch("/colors")
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+
+            data.forEach((color, i) => {
+              color = "background-color: #" + color
+              console.log(color)
+              data[i] = color
+
+            });
+
+            document.querySelector('#color-table').innerHTML = `
+                <thead>
+                    <th id="color-1" style="${data[0]}"></th>
+                    <th id="color-2" style="${data[1]}"></th>
+                    <th id="color-3" style="${data[2]}"></th>
+                    <th id="color-4" style="${data[3]}"</th>
+                    <th id="color-5" style="${data[4]}"></th>
+                </thead>
+            `;
+        })
+}
